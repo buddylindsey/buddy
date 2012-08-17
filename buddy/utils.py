5e1b8f3eddc1fd43074df8a443a2da0880d5b912
Buddy Lindsey <buddy@buddylindsey.com>
2012-08-16 19:52:04 -0500

    initial commit

---
+++ buddy/utils.py
@@ -0,0 +1,4 @@
+from datetime import datetime
+
+def datetime_string():
+    return datetime.now().strftime("%Y%m%d%H%M")
---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='buddy',
+    description='Helpers and Utilities for Django',
+    version='0.1',
+    packages=['buddy'],
+    author='Buddy Lindsey',
+    author_email='percent20@gmail.com',
+    url='https://github.com/buddylindsey/buddy',
+    license=open("LICENSE").read(),
+    install_requires='',
+    package_data={'buddy': ['static/browserid/*.js']},
+)
